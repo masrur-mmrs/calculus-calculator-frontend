1e8d49d43740efd63f5d35a447ed3667027de3cd
masrur-mmrs <masrur.mmrs@gmail.com>
2025-03-31 23:58:13 +0600

    minor change

--- src/app/(main)/integral/_components/BoundSelector.tsx
+++ src/app/(main)/integral/_components/BoundSelector.tsx
@@ -35,29 +35,29 @@ const BoundSelector: React.FC<BoundSelectorPropsType> = ({bound}) => {
     }
 
     return (
-            <div className="flex flex-col ml-2 border-2 border-white-mode-blue dark:border-muted-teal rounded-xl mr-2">
-                <label htmlFor="wrt" className="block text-sm font-bold m-1.5 border-b-2 border-white-mode-blue dark:border-muted-teal text-center">
-                    {bound[0].toUpperCase()}B
-                </label>
-                <select
-                    id="wrt"
-                    onChange={handleSelectionChange}
-                    className="h-min w-min p-1.5 ml-0.5 font-mono text-md italic"
-                >
-                    <option value="" aria-disabled defaultChecked>∅</option>
-                    <option value="0">0</option>
-                    <option value="\infty">∞</option>
-                    <option value="-\infty">-∞</option>
-                    <option value="e">e</option>
-                    <option value="\pi">π</option>
-                    <option value="num">✎</option>
-                </select>
-                {show&&<input 
-                    placeholder="0"
-                    onChange={handleInputChange}
-                    className="m-1.5 w-10 text-center border-t-2 border-white-mode-blue dark:border-muted-teal"
-                />}
-            </div>
+        <div className="flex flex-col ml-2 border-2 border-white-mode-blue dark:border-muted-teal rounded-xl mr-2">
+            <label htmlFor={bound} className="block text-sm font-bold m-1.5 border-b-2 border-white-mode-blue dark:border-muted-teal text-center">
+                {(bound === "upper")?"UPB":"LWB"}
+            </label>
+            <select
+                id={bound}
+                onChange={handleSelectionChange}
+                className="h-min w-min p-1.5 ml-0.5 font-mono text-md italic"
+            >
+                <option value="" aria-disabled defaultChecked>∅</option>
+                <option value="0">0</option>
+                <option value="\infty">∞</option>
+                <option value="-\infty">-∞</option>
+                <option value="e">e</option>
+                <option value="\pi">π</option>
+                <option value="num">✎</option>
+            </select>
+            {show&&<input 
+                placeholder="0"
+                onChange={handleInputChange}
+                className="m-1.5 w-10 text-center border-t-2 border-white-mode-blue dark:border-muted-teal"
+            />}
+        </div>
     )
 }
 
