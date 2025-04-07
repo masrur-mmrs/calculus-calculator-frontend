1ac16d865feb9e61e87585a637b348a668a13de8
masrur-mmrs <masrur.mmrs@gmail.com>
2025-04-08 03:13:46 +0600

    fixed sd select

--- src/app/(main)/basic/_components/BasicOptions.tsx
+++ src/app/(main)/basic/_components/BasicOptions.tsx
@@ -4,29 +4,14 @@ import { setSd } from '@/redux/slices/sdSlice';
 import { useDispatch, useSelector } from 'react-redux';
 
 const BasicOptions: React.FC = () => {
-    const sd = useSelector((state: RootState) => state.ood.value)
+    const sd = useSelector((state: RootState) => state.sd.value)
     const dispatch = useDispatch<AppDispatch>();
 
     return (
         <div className="flex flex-row w-max mt-5">
-            {/* <div className="flex flex-col border-2 border-white-mode-blue dark:border-muted-teal rounded-xl">
-                <label htmlFor="wrt" className="block text-sm text-center font-bold m-1.5 border-b-2 border-white-mode-blue dark:border-muted-teal">
-                    D⇔R
-                </label>
-                <select
-                    id="wrt"
-                    // value={wrt}
-                    // onChange={(event) => dispatch(setWrt(event.target.value))}
-                    className="h-min w-min p-1.5 ml-0.5 font-mono text-md italic"
-                >
-                    <option value="degree" defaultChecked>Deg</option>
-                    <option value="rad">Rad</option>
-                </select>
-            </div> */}
             <div className="flex flex-col border-2 border-white-mode-blue dark:border-muted-teal rounded-xl">
-                <label htmlFor="wrt" className="block text-sm text-center font-bold m-1.5 border-b-2 border-white-mode-blue dark:border-muted-teal">
+                <label htmlFor="sd" className="block text-sm text-center font-bold m-1.5 border-b-2 border-white-mode-blue dark:border-muted-teal">
                     S⇔D
-                    {/* {sd} */}
                 </label>
                 <select
                     id="sd"
